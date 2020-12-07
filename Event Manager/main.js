abd8525211d8c733957c6fff6672be3990059fbb
Mark Pedroso <pedrosomark98@gmail.com>
2020-12-07 13:45:47 -0500

    Register and go back to login buttons work now

---
+++ Event Manager/main.js
@@ -0,0 +1,30 @@
+function hideOrShow( elementId, showState )
+{
+	var vis = "visible";
+	var dis = "block";
+	if( !showState )
+	{
+		vis = "hidden";
+		dis = "none";
+	}
+	document.getElementById( elementId ).style.visibility = vis;
+	document.getElementById( elementId ).style.display = dis;
+}
+
+function toRegister()
+{
+	hideOrShow("registerDiv", true);
+	hideOrShow("loginDiv", false);
+}
+
+function doLogout()
+{
+	/*userId = 0;
+	firstName = "";
+	lastName = "";
+
+	hideOrShow("loggedInDiv", false);
+	hideOrShow("accessUIDiv", false);
+	hideOrShow("editDiv", false);*/
+	hideOrShow("loginDiv", true);
+}
