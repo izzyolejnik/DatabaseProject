d7d3a31b12d3aec9fee010b1048cd64d7bce73e3
alexogilbee <aogilbee0699@gmail.com>
2020-12-09 02:21:15 -0500

    admin search self-created events

--- Event Manager/main.js
+++ Event Manager/main.js
@@ -152,3 +152,63 @@ function searchUser(str)
 		document.getElementById("userSearchResult").innerHTML = err.message;
 	}
 }
+
+function showEvents(active)
+{
+	var urlBase = "/DatabaseProject/Event%20Manager";
+	// irrelevant? var jsonPayload = '{"search" : "' + search + '"}';
+	var url = urlBase + '/searchMyEvents.php?active=' + active;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		// Send the json info to php
+		xhr.send(/*jsonPayload*/);
+
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				var jsonObject = JSON.parse(xhr.responseText);
+
+				temp = 0;
+				
+				// clear the table
+				for (var i = document.getElementById("adminTable").rows.length; i > 1; i--)
+				{
+					document.getElementById("adminTable").deleteRow(i -1);
+				}
+				
+				while (temp < jsonObject.Name.length)
+				{
+					var table = document.getElementById('adminTable');
+
+					// add a new row to the bottom of the table, then fill with information
+					var row = table.insertRow(-1);
+
+					var cell1 =row.insertCell(0);
+					var cell2 =row.insertCell(1);
+					var cell3 =row.insertCell(2);
+					var cell4 =row.insertCell(3);
+					var cell5 =row.insertCell(4);
+					var cell6 =row.insertCell(5);
+
+					cell1.innerHTML = jsonObject.Name[temp];
+					cell2.innerHTML = jsonObject.Title[temp];
+					cell3.innerHTML = jsonObject.Description[temp];
+					cell4.innerHTML = jsonObject.Url[temp];
+					cell5.innerHTML = jsonObject.Start[temp];
+					cell6.innerHTML = jsonObject.End[temp];
+					temp++;
+				}
+
+			}
+		};
+	}
+	catch(err)
+	{
+		document.getElementById("adminSearchResult").innerHTML = err.message;
+	}
+}
